0327389976c9fcf04a079482a2b7f88e5d71dce2
Gabriel Santana <santana.gabrielmenezes+github@gmail.com>
2026-09-21 12:56:30 -0300

    aula 03.02

--- deteccao_maos.py
+++ deteccao_maos.py
@@ -12,25 +12,38 @@ resolucao_y = 720
 camera.set(cv2.CAP_PROP_FRAME_WIDTH, resolucao_x)
 camera.set(cv2.CAP_PROP_FRAME_HEIGHT, resolucao_y)
 
-def encontra_coordenadas_maos(img):
+def encontra_coordenadas_maos(img, lado_invertido = False):
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
     resultado = maos.process(img_rgb)
+    todas_maos = []
 
     if resultado.multi_hand_landmarks:
-        for marcacoes_maos in resultado.multi_hand_landmarks:
+        for lado_mao, marcacoes_maos in zip(resultado.multi_handedness, resultado.multi_hand_landmarks): 
+            info_mao = {}
             coordenadas = []
             for marcacao in marcacoes_maos.landmark:
                 coord_x, coord_y, coord_z = int(marcacao.x * resolucao_x),int(marcacao.y * resolucao_y), int(marcacao.z * resolucao_x)
                 coordenadas.append((coord_x, coord_y, coord_z))
 
-            mp_desenho.draw_landmarks(img, marcacoes_maos, mp_maos.HAND_CONNECTIONS)
+                info_mao['coordenadas'] = coordenadas
+                if lado_invertido:
+                    if lado_mao.classification[0].label == 'Left':
+                        info_mao['lado'] = 'Right'
+                    else: 
+                        info_mao['lado'] = 'Left'
+                else: 
+                    info_mao['lado'] = lado_mao.classification[0].label
+
+                print(info_mao['lado'])
+
+                todas_maos.append(info_mao)
+                mp_desenho.draw_landmarks(img, marcacoes_maos, mp_maos.HAND_CONNECTIONS)
 
     return img, todas_maos
 
-
 while True: 
     sucesso, img = camera.read()
+    img = cv2.flip(img)
 
     img, todas_maos = encontra_coordenadas_maos(img)
 
