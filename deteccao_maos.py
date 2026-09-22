03efb34f6749e3a247239e688a4996c17af32b48
santana.gabrielmenezes <santana.gabrielmenezes+github@gmail.com>
2026-09-21 21:37:07 -0300

    aula 04.07

--- deteccao_maos.py
+++ deteccao_maos.py
@@ -1,9 +1,16 @@
 import cv2
 import mediapipe as mp
+import os
+
+BRANCO = (255,255,255)
+PRETO = (0,0,0)
+AZUL = (255,0,0)
+VERDE = (0,255,0)
+VERMELHO = (0,0,255)
+AZUL_CLARO = (255,255,0)
 
 mp_maos = mp.solutions.hands
 mp_desenho = mp.solutions.drawing_utils 
-
 maos = mp_maos.Hands()
 
 camera = cv2.VideoCapture(0)
@@ -11,6 +18,15 @@ resolucao_x = 1280
 resolucao_y = 720
 camera.set(cv2.CAP_PROP_FRAME_WIDTH, resolucao_x)
 camera.set(cv2.CAP_PROP_FRAME_HEIGHT, resolucao_y)
+bloco_notas = False
+chrome = False
+calculadora = False
+teclas = [['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+          ['A','S','D','F','G','H','J','K','L'],
+          ['Z','X','C','V','B','N','M', ',','.',' ']]
+offset = 50
+contador = 0
+texto = '>'
 
 def encontra_coordenadas_maos(img, lado_invertido = False):
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -34,22 +50,76 @@ def encontra_coordenadas_maos(img, lado_invertido = False):
                 else: 
                     info_mao['lado'] = lado_mao.classification[0].label
 
-                print(info_mao['lado'])
-
                 todas_maos.append(info_mao)
                 mp_desenho.draw_landmarks(img, marcacoes_maos, mp_maos.HAND_CONNECTIONS)
 
     return img, todas_maos
 
+
+def dedos_levantados(mao):
+    dedos = []
+    for ponta_dedo in [8,12,16,20]:
+        if mao['coordenadas'][ponta_dedo][1] < mao['coordenadas'][ponta_dedo-2][1]:
+           dedos.append(True)
+        else:
+           dedos.append(False)
+
+    return dedos
+
+def imprime_botoes(img, posicao, letra, tamanho = 50, cor_retangulo = BRANCO):
+    cv2.rectangle(img, posicao, (posicao[0]+tamanho, posicao[1]+tamanho), cor_retangulo, cv2.FILLED)
+    cv2.rectangle(img, posicao, (posicao[0]+tamanho, posicao[1]+tamanho), AZUL, 1)
+    cv2.putText(img, letra, (posicao[0]+15,posicao[1]+30), cv2.FONT_HERSHEY_COMPLEX, 1, PRETO, 2)
+    return img
+
 while True: 
     sucesso, img = camera.read()
-    img = cv2.flip(img)
+    img = cv2.flip(img, 1)
 
     img, todas_maos = encontra_coordenadas_maos(img)
 
-    cv2.imshow('Imagem', img)
+    cv2.rectangle(img, (50,50), (100,100), BRANCO, cv2.FILLED)
+    cv2.putText(img, 'Q', (65,85), cv2.FONT_HERSHEY_COMPLEX, 1, PRETO, 2)
 
+    if len(todas_maos) == 1:
+        info_dedos_mao1 = dedos_levantados(todas_maos[0])
+        if todas_maos[0]['lado'] == 'Left':
+            indicador_x, indicador_y, indicador_z = todas_maos[0]['coordenadas'][8]
+            cv2.putText(img, f'Distancia camera: {indicador_z}', (850, 50), cv2.FONT_HERSHEY_COMPLEX, 1, BRANCO, 2)
+            for indice_linha, linha_teclado in enumerate(teclas):
+                for indice, letra in enumerate(linha_teclado):
+                    if sum(info_dedos_mao1) <= 1:
+                        letra = letra.lower()
+                    img = imprime_botoes(img, (offset + indice * 80, offset + indice_linha * 80), letra)
+                    if offset+indice*80 < indicador_x < 100+indice*80 and offset+indice_linha*80 < indicador_y < 100+indice_linha*80:
+                        img = imprime_botoes(img, (offset+indice*80, offset+indice_linha*80),letra, cor_retangulo=VERDE)
+                        if indicador_z < -85:
+                            contador = 1
+                            escreve = letra
+                            img = imprime_botoes(img, (offset+indice*80, offset+indice_linha*80),letra, cor_retangulo=AZUL_CLARO)
+            if contador:
+                contador += 1
+                if contador == 3:
+                    texto += escreve
+                    contador = 0
+
+        if todas_maos[0]['lado'] == 'Right':
+            if info_dedos_mao1 == [True, False, False, False] and bloco_notas == False:
+                bloco_notas = True
+                os.startfile(r'C:\Windows\system32\notepad.exe')
+            if info_dedos_mao1 == [True, True, False, False] and chrome == False:
+                chrome = True
+                os.startfile(r'C:\Program Files\Google\Chrome\Application\chrome.exe')
+            if info_dedos_mao1 == [True, True, True, False] and calculadora == False:
+                calculadora = True
+                os.startfile(r'C:\Windows\system32\calc.exe')
+            if info_dedos_mao1 == [False, False, False, False] and bloco_notas == True:
+                bloco_notas = False
+                os.system('TASKKILL /IM notepad.exe')
+            if info_dedos_mao1 == [True,False,False,True]:
+                break
+
+    cv2.imshow("Imagem", img)
     tecla = cv2.waitKey(1)
-
     if tecla == 27:
         break
