7ddf0b9f5d05b3ed98b477b2f4479a398c31f8a5
santana.gabrielmenezes <santana.gabrielmenezes+github@gmail.com>
2026-09-22 20:49:39 -0300

    final

--- deteccao_maos.py
+++ deteccao_maos.py
@@ -1,6 +1,7 @@
 import cv2
 import mediapipe as mp
 import os
+import numpy as np
 from time import sleep
 from pynput.keyboard import Controller
 
@@ -30,6 +31,10 @@ offset = 50
 contador = 0
 texto = '>'
 teclado = Controller()
+img_quadro = np.ones((resolucao_y, resolucao_x, 3), np.uint8)*255
+cor_pincel = (255,0,0)
+espessura_pincel = 7
+x_quadro, y_quadro = 0, 0
 
 def encontra_coordenadas_maos(img, lado_invertido = False):
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -129,10 +134,45 @@ while True:
             if info_dedos_mao1 == [True, False, False, True]:
                 break
 
+    if len(todas_maos) == 2:
+        info_dedos_mao1 = dedos_levantados(todas_maos[0])
+        info_dedos_mao2 = dedos_levantados(todas_maos[1])
+
+        indicador_x, indicador_y, indicador_z = todas_maos[0]['coordenadas'][8]
+
+        if sum(info_dedos_mao2)==1:
+            cor_pincel = AZUL
+        elif sum(info_dedos_mao2) ==2:
+            cor_pincel = VERDE
+        elif sum(info_dedos_mao2) == 3:
+            cor_pincel = VERMELHO
+        elif sum(info_dedos_mao2) == 4:
+            cor_pincel = BRANCO
+        else:
+            img_quadro = np.ones((resolucao_y, resolucao_x, 3), np.uint8)*255
+
+        espessura_pincel = int(abs(indicador_z))//3+5
+        cv2.circle(img, (indicador_x, indicador_y), espessura_pincel, cor_pincel, cv2.FILLED)
+
+        if info_dedos_mao1 == [True, False, False, False]:
+            if x_quadro == 0 and y_quadro == 0:
+                x_quadro, y_quadro = indicador_x, indicador_y
+
+            cv2.line(img_quadro, (x_quadro, y_quadro), (indicador_x, indicador_y), cor_pincel, espessura_pincel)
+
+            x_quadro, y_quadro = indicador_x, indicador_y
+        else:
+            x_quadro, y_quadro = 0, 0
+
+        img = cv2.addWeighted(img, 1, img_quadro, 0.2, 0)
+
     cv2.imshow("Imagem", img)
+    cv2.imshow('Quadro', img_quadro)
     tecla = cv2.waitKey(1)
     if tecla == 27:
         break
 
 with open('texto.txt', 'w') as arquivo:
     arquivo.write(texto)
+
+cv2.imwrite('quadro.png', img_quadro)
